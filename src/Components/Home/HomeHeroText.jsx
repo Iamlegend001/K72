cd07c102b8048a3ea1ae14c68de8ed564e402f21
Arka <arkapravasantra17@gmail.com>
2025-09-08 12:52:12 +0530

    multiplae animatin done

--- src/Components/Home/HomeHeroText.jsx
+++ src/Components/Home/HomeHeroText.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import Video from "./Video";
 
 const HomeHeroText = () => {
   return (
-    <div className="font-[font2]">
-      <div >L'étincelle</div>
-      <div>quigénère</div>
-      <div>la créativité</div>
+    <div className="font-[font1] text-center pt-5">
+      <div className="text-[9.5vw] justify-center uppercase leading-[8vw]">L'étincelle</div>
+      <div className="text-[9.5vw] justify-center uppercase  flex items-first leading-[8vw]">
+        qu{" "}
+        <div className="h-[7vw] w-[16vw] rounded-full overflow-hidden -mt-3">
+          <Video />
+        </div>
+        igénère
+      </div>
+      <div className="text-[9.5vw] justify-center uppercase leading-[8vw]">la créativité</div>
     </div>
   );
 };
--- src/Pages/Agence.jsx
+++ src/Pages/Agence.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import React, { useRef } from "react";
 
 const Agence = () => {
-  return (
-    <div>Agence</div>
-  )
-}
+  gsap.registerPlugin(ScrollTrigger);
+  const imageDivRef = useRef(null);
+  const imageRef = useRef(null);
 
-export default Agence
+  const imageArray = [
+    "https://k72.ca/uploads/teamMembers/Chantal_640X960-640x960.jpg",
+    "https://k72.ca/uploads/teamMembers/Isa_640X960-640x960.jpg",
+    "https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Olivier_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Lawrence_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/HugoJoseph_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/ChantalG_480x640-480x640.jpg",
+  ];
+
+  useGSAP(function () {
+    gsap.to(imageDivRef.current, {
+      scrollTrigger: {
+        trigger: imageDivRef.current,
+        markers: true,
+        start: "top 36%",
+        end: "top -70%",
+        scrub: true,
+        pin: true,
+        onUpdate: function (elem) {
+          let imageIndex;
+          if (elem.progress < 1) {
+            imageIndex = Math.floor(elem.progress * imageArray.length);
+          } else {
+            imageIndex = imageArray.length - 1;
+          }
+
+          imageRef.current.src = imageArray[imageIndex];
+        },
+      },
+    });
+  });
+
+  return (
+    <div>
+      {" "}
+      <div className="section1">
+        <div
+          ref={imageDivRef}
+          className="h-[20vw] w-[15vw] overflow-hidden rounded-4xl absolute top-96 left-[40vw]"
+        >
+          <img
+            className="h-full w-full object-cover"
+            ref={imageRef}
+            src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg"
+            alt=""
+          />
+        </div>
+        <div className=" relative font-[font2]">
+          <div className="mt-[55vh]">
+            <h1 className="text-[20vw] text-center uppercase leading-[17vw]">
+              Soixan7e <br />
+              Douze
+            </h1>
+          </div>
+          <div className="pl-[40%] mt-20">
+            <p className="text-6xl ">
+              &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Notre
+              curiosité nourrit notre créativité. On reste humbles et on dit non
+              aux gros egos, même le vôtre. Une marque est vivante. Elle a des
+              valeurs, une personnalité, une histoire. Si on oublie ça, on peut
+              faire de bons chiffres à court terme, mais on la tue à long terme.
+              C’est pour ça qu’on s’engage à donner de la perspective, pour
+              bâtir des marques influentes.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="section2 h-screen"></div>
+    </div>
+  );
+};
+
+export default Agence;
