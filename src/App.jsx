9ef2a6381e92099a967dafe78e94bb3636b6f2a8
Arka <arkapravasantra17@gmail.com>
2025-10-08 23:12:06 +0530

    matter

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import MainRoutes from "./Routes/MainRoutes";
-
+import Navbar from "./Components/Navigation/Navbar";
 
 const App = () => {
-
-
   return (
     <>
-
-
+      <Navbar />
       <MainRoutes />
     </>
   );
